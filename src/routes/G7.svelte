010339694115edf4456e77513e026ee168830523
junxian428 <holeyiho@gmail.com>
2022-08-05 13:59:52 +0800

    call Laravel API to display image

---
+++ src/routes/G7.svelte
@@ -0,0 +1,16 @@
+<script>
+	const fetchImage = (async () => {
+		const response = await fetch('http://localhost:8000/api/PhotoAPI')
+    return await response.json()
+	})()
+
+    console.log(fetchImage)
+</script>
+
+{#await fetchImage}
+	<p>...waiting</p>
+{:then data}
+	<img src={data[0]['image']} alt="Image" />
+{:catch error}
+	<p>An error occurred!</p>
+{/await}
